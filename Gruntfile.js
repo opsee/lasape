2614849575ed0c6af1f980d122cb9504478a4714
Sara Bee <sara.bainbridge@gmail.com>
2016-02-22 17:15:04 -0500

    Remove unused stuff from gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,28 +21,6 @@ module.exports = function(grunt) {
       },
       opseeBower:{
         command:'bower update seedling'
-      },
-      docker:{
-        command:'docker build -t quay.io/opsee/lasape .'
-      },
-    },
-    connect: {
-      server: {
-        options: {
-          port: 8000,
-          middleware: function(connect, options, middlewares) {
-            var rules = [
-                "!\\.html|\\.js|\\.css|\\.svg|\\.jp(e?)g|\\.png|\\.woff|\\.tiff|\\.gif$ /index.html"
-            ];
-            middlewares.unshift( rewrite( rules ) );
-            return middlewares;
-          }
-        }
-      }
-    },
-    open:{
-      dev:{
-        path:'http://localhost:8000'
       }
     },
     copy:{
@@ -56,14 +34,6 @@ module.exports = function(grunt) {
         ]
       }
     },
-    browserify:{
-      slate:{
-        src:['node_modules/slate/src/exports.js'],
-        dest:'public/js/dist/npm-deps.js',
-        options:{
-        }
-      }
-    },
     autoprefixer:{
       single_file:{
         options:{
@@ -73,57 +43,13 @@ module.exports = function(grunt) {
         dest:'css/dist/style.min.css'
       }
     },
-    babel:{
-      options:{
-        sourceMap:false
-      },
-      dist:{
-        files:[{
-          expand:true,
-          cwd:'public/js/es6',
-          src:['**/*.js'],
-          dest:'public/js/src',
-          ext:'.js'
-        }]
-      }
-    },
-    preprocess:{
-      html:{
-        expand:true,
-        cwd:'public/js/es6',
-        src:['**/*.html'],
-        dest:'public/js/src',
-        ext:'.html'
-      }
-    },
     watch:{
-      grunt:{
-        files:['Gruntfile.js'],
-        tasks:['browserify']
-      },
-      appIndex:{
-        options:{
-          livereload:true
-        },
-        files:['_site/index.html'],
-        tasks:['shell:jekyll','copy']
-      },
       emailFiles:{
         options:{
           livereload:true
         },
         files:['_site/email/**/*.**', '_site/_layouts/**/*.**'],
         tasks:['shell:jekyll','emailBuilder:inline']
-      },
-      babel:{
-        files:['public/js/es6/**/*.js'],
-        tasks:['processbabel']
-      },
-      srcScripts:{
-        options:{
-           livereload:true
-         },
-        files:['js/public/src/**/*.js'],
       },
       sass:{
         files:['scss/**/*.scss'],
@@ -138,16 +64,6 @@ module.exports = function(grunt) {
           livereload:true
         },
         files:['public/css/**/*.css']
-      },
-      preprocess:{
-        files:['public/js/es6/**/*.html'],
-        tasks:['processhtml']
-      },
-      pages:{
-        options:{
-          livereload:true
-        },
-        files:['public/js/src/**/*.html']
       }
     },
     compass:{
